575f070780e988a4898141f3b07dd3ceeb339552
Ying Lu <rosebudflyaway@gmail.com>
2016-11-27 15:32:45 -0800

    Fix a bug in avel2qdot, reported by professor Evan Drumwright

--- engine/funcs/qt/avel2qdot.m
+++ engine/funcs/qt/avel2qdot.m
@@ -3,7 +3,7 @@
 function qdot = avel2qdot(q, w)
 
   % determine G
-  G = [-q(2) q(1) -q(4) q(3); -q(3) q(4) q(1) -q(2); -q(4) -q(3) -q(2) q(1)];
+  G = [-q(2) q(1) -q(4) q(3); -q(3) q(4) q(1) -q(2); -q(4) -q(3) q(2) q(1)];
 
   % compute quaternion
   qdot = 0.5*G'*w;
